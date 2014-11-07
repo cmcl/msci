ba58ed82fb70dbd93362333f304fefae8e0d5a0c
Craig McLaughlin <mr_mcl@live.co.uk>
2014-11-07 22:22:39 +0000

    Added the majority of the CP rules and notation for the process grammar.

--- Coq Developments/msci/CP.v
+++ Coq Developments/msci/CP.v
@@ -28,20 +28,23 @@ Inductive prop : Type :=
   | pp_zero : prop (* unit for plus *)
   | pp_top : prop (* unit for with *).
 
-(** Define some friendly notation. *)
-Notation "A ⨂ B" := (pp_times A B) (at level 68, left associativity)
+Hint Constructors prop.
+
+(** Define some friendly notation. Level 48 to have higher precedence than
+    the ~ notation for environments; prevents need for parens. *)
+Notation "A ⨂ B" := (pp_times A B) (at level 48, left associativity)
                                          : cp_scope.
-Notation "A ⅋ B" := (pp_par A B) (at level 68, left associativity)
+Notation "A ⅋ B" := (pp_par A B) (at level 48, left associativity)
                                      : cp_scope.
-Notation "A ⨁ B" := (pp_plus A B) (at level 68, left associativity)
+Notation "A ⨁ B" := (pp_plus A B) (at level 48, left associativity)
                                        : cp_scope.
-Notation "A & B" := (pp_with A B) (at level 68, left associativity)
+Notation "A & B" := (pp_with A B) (at level 48, left associativity)
                                        : cp_scope.
-Notation "'!' A" := (pp_accept A) (at level 68, left associativity)
+Notation "'!' A" := (pp_accept A) (at level 48, left associativity)
                                   : cp_scope.
-Notation "'?' A" := (pp_request A) (at level 68, left associativity)
+Notation "'?' A" := (pp_request A) (at level 48, left associativity)
                                    : cp_scope.
-Reserved Notation "'¬' A" (at level 67, left associativity).
+Reserved Notation "'¬' A" (at level 47, left associativity).
 
 Delimit Scope cp_scope with cp.
 Open Scope cp_scope.
@@ -74,7 +77,7 @@ Qed.
 
 (** Substitution of proposition [A] for [X] in [B] is denoted by
     {{ A // X }} B -- double syntax used to get Coq to accept it. *)
-Reserved Notation "'{{' A '//' X '}}' B" (at level 66, left associativity).
+Reserved Notation "'{{' A '//' X '}}' B" (at level 46, left associativity).
 
 (** Substitution structural recurses over the definition of a proposition.
     Note that there is an assumption (TODO: Enforce this?) that binders are
@@ -111,8 +114,8 @@ Qed.
 Inductive proc : Set :=
   | p_link : atom -> atom -> proc
   | p_par : atom -> prop -> proc -> proc -> proc
-  | p_output : atom -> atom -> proc -> proc
-  | p_input : atom -> atom -> proc
+  | p_output : atom -> atom -> proc -> proc -> proc
+  | p_input : atom -> atom -> proc -> proc
   | p_left : atom -> proc -> proc
   | p_right : atom -> proc -> proc
   | p_choice : atom -> proc -> proc -> proc
@@ -124,16 +127,124 @@ Inductive proc : Set :=
   | p_empin : atom -> proc -> proc
   | p_empcho : atom -> proc.
 
+Hint Constructors proc.
+
+(** Some helpful notations. *)
+Notation "x ⟷ y" := (p_link x y) (at level 68) : cp_scope.
+Notation "'ν' x ':' A '→' P '|' Q" := (p_par x A P Q) (at level 68, x ident,
+                                                       right associativity)
+                                                      : cp_scope.
+(** Example of using parallel composition :
+
+    Parameter y : atom.
+    Print Grammar constr.
+    Check ν y : ((pp_var 0) ⅋ (pp_dvar 1)) →
+             p_empin y (p_empout y) | p_empout y.
+*)
+(** Change of notation from the paper; Coq doesn't seem to like the x coming
+    first. *)
+Notation "'[' y ']' x '→' P '|' Q" := (p_output x y P Q) (at level 68,
+                                                          y ident, x ident,
+                                                         right associativity)
+                                                         : cp_scope.
+(** We use ⟨⟩ instead of () in the input cases. *)
+Notation "'⟨' y '⟩' x '→' P" := (p_input x y P) (at level 68, y ident,
+                                                right associativity)
+                                                : cp_scope.
+
+Notation "x '[inl]' → P" := (p_left x P) (at level 68,
+                                          right associativity) : cp_scope.
+Notation "x '[inr]' → P" := (p_right x P) (at level 68,
+                                          right associativity) : cp_scope.
+Notation "x 'CASE' P 'OR' Q" := (p_choice x P Q) (at level 68,
+                                                  right associativity)
+                                                 : cp_scope.
+Notation "'!' x '⟨' y '⟩' → P" := (p_accept x y P) (at level 68, x ident,
+                                                    y ident,
+                                                    right associativity)
+                                                   : cp_scope.
+Notation "'?' x '[' y ']' → P" := (p_request x y P) (at level 68, x ident,
+                                                     y ident,
+                                                     right associativity)
+                                                    : cp_scope.
+Notation "x '→' 0" := (p_empout x) (at level 68) : cp_scope.
+Notation "⟨⟩ x → P" := (p_empin x P) (at level 68, x ident,
+                                      right associativity) : cp_scope.
+Notation "x 'CASE' 0" := (p_empcho x) (at level 68) : cp_scope.
+
+(** Example using some notations (demonstrates the right associativity of the
+    rules):
+
+Parameter x y: atom.
+Check ν x : (pp_var 0) ⨁ (pp_var 1) → x ⟷ y | x → 0.
+
+*)
+
 (** Environments for the process calculus are mappings of atoms to
     propositions. *)
 Definition penv := list (atom * prop).
 
-(** Well-formed process environments are defined as follows: An environment
-    [env] is well-formed ([wf_penv env]) if all names are unique. *)
-Inductive wf_penv : penv -> Prop :=
-  | wf_penv_nil : wf_penv nil
-  | wf_penv_cons:
-      forall (x : atom) (p : prop) (xs : penv)
-             (NIN: x `notin` dom xs),
-        wf_penv ((x,p) :: xs).
+(** Encoding an environment as all requests; for the server accept process
+    rule. *)
+Inductive all_requests : penv -> Prop :=
+  | all_requests_nil : all_requests nil
+  | all_requests_cons : forall Γ x A (REQS: all_requests Γ),
+                          all_requests (Γ ++ (x ~ ? A)).
 
+Reserved Notation "P '⊢cp' Γ" (at level 69).
+
+(** The uniqueness assumption is necessary to ensure environments are only
+    combined if they contain distinct names.
+    Note in some cases we utilise cofinite quantification to provide a
+    suitably fresh name for some channels. Some cases could be written has
+    x `notin` Γ for some x, Γ but I elected to maintain uniq assumptions
+    wherever possible to keep the development symmetrical (in theory, this
+    could help proofs since all rules follow a similar structure).
+*)
+Inductive cp_rules : proc -> penv -> Prop :=
+  | cp_fwd : forall x w A (NEQ: x <> w), w ⟷ x ⊢cp ((w ~ ¬ A) ++ (x ~ A))
+  | cp_cut : forall P Q x A Γ Δ
+                    (UN: uniq (Γ ++ Δ ++ (x ~ A)))
+                    (CPP: P ⊢cp Γ ++ (x ~ A))
+                    (CPQ: Q ⊢cp Δ ++ (x ~ ¬A)),
+               ν x : A → P | Q ⊢cp Γ ++ Δ
+  | cp_output : forall P Q Γ Δ x y A B
+                       (NING: y `notin` dom Γ)
+                       (UN: uniq (Γ ++ Δ ++ (x ~ A ⨂ B)))
+                       (CPP: P ⊢cp Γ ++ (y ~ A))
+                       (CPQ: Q ⊢cp Δ ++ (x ~ B)),
+                  [y]x → P | Q ⊢cp Γ ++ Δ ++ (x ~ A ⨂ B)
+  | cp_input : forall P Γ x y A B
+                      (UN: uniq (Γ ++ (y ~ A) ++ (x ~ A ⅋ B)))
+                      (CPP: P ⊢cp Γ ++ (y ~ A) ++ (x ~ B)),
+                 ⟨y⟩x → P ⊢cp Γ ++ (x ~ A ⅋ B)
+  | cp_left : forall P Γ x A B
+                     (UN: uniq (Γ ++ (x ~ A ⨁ B)))
+                     (CPP: P ⊢cp Γ ++ (x ~ A)),
+                x[inl] → P ⊢cp Γ ++ (x ~ A ⨁ B)
+  | cp_right : forall P Γ x A B
+                     (UN: uniq (Γ ++ (x ~ A ⨁ B)))
+                     (CPP: P ⊢cp Γ ++ (x ~ B)),
+                x[inr] → P ⊢cp Γ ++ (x ~ A ⨁ B)
+  | cp_choice : forall P Q Γ x A B
+                       (UN: uniq (Γ ++ (x ~ A & B)))
+                       (CPP: P ⊢cp Γ ++ (x ~ A))
+                       (CPQ: Q ⊢cp Γ ++ (x ~ B)),
+                  x CASE P OR Q ⊢cp Γ ++ (x ~ A & B)
+  | cp_accept : forall P Γ x y A
+                       (NING: y `notin` dom Γ)
+                       (REQS: all_requests Γ)
+                       (UN: uniq (Γ ++ (x ~ ! A)))
+                       (CPP: P ⊢cp Γ ++ (y ~ A)),
+                  !x⟨y⟩ → P ⊢cp Γ ++ (x ~ ! A)
+  | cp_request : forall P Γ x y A
+                        (NING: y `notin` dom Γ)
+                        (UN: uniq (Γ ++ (x ~ ? A)))
+                        (CPP: P ⊢cp Γ ++ (y ~ A)),
+                   ? x[y] → P ⊢cp Γ ++ (x ~ ? A)
+  | cp_empout : forall x, x → 0 ⊢cp (x ~ pp_one)
+  | cp_empin : forall P Γ x (UN: uniq (Γ ++ (x ~ pp_bot))) (CPP: P ⊢cp Γ),
+                 ⟨⟩ x → P ⊢cp Γ ++ (x ~ pp_bot)
+  | cp_empcho : forall Γ x (UN: uniq (Γ ++ (x ~ pp_top))),
+                  x CASE 0 ⊢cp Γ ++ (x ~ pp_top)
+where "P '⊢cp' Γ" := (cp_rules P Γ) : cp_scope.
